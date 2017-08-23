980e9fdf6400d5c5b9f27993b090300715277d55
liyl <295697141@qq.com>
2017-08-23 11:43:53 +0800

    first commit

---
+++ config/config.default.js
@@ -0,0 +1,12 @@
+'use strict';
+
+module.exports = appInfo => {
+  const config = {};
+
+  // should change to your own
+  config.keys = appInfo.name + '_1503458409524_3029';
+
+  // add your config here
+
+  return config;
+};
